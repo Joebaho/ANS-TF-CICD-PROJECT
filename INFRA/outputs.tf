e5be89d6d1605768b61dc370a55038ce012f3237
Joebaho <josephmbatchou2@gmail.com>
2025-05-14 21:58:18 -0700

    changing outputs

--- INFRA/outputs.tf
+++ INFRA/outputs.tf
@@ -3,23 +3,27 @@
 output "controller_ip" {
   value = aws_instance.controller.public_ip
 }
-#public IPS of workers nodes
-output "worker_ips" {
-  value = concat(
-    aws_instance.ubuntu_workers[*].public_ip,
-    aws_instance.amazon_linux_workers[*].public_ip
-  )
-}
-#Private IPs of Workers node
-output "worker_Private_ips" {
-  value = concat(
-    aws_instance.ubuntu_workers[*].private_ip,
-    aws_instance.amazon_linux_workers[*].private_ip
-  )
-}
 # key pair output 
 output "private_key" {
   value     = tls_private_key.example.private_key_pem
   sensitive = true
+}
+#outpus 
+#output of the public Ip 
+output "amazon_worker_ips" {
+  value = aws_instance.amazon_linux_workers[*].public_ip
+}
+#output of the pivate Ip 
+output "ubuntu_worker_ips" {
+  value = aws_instance.ubuntu_workers[*].public_ip
+}
+#private IPS
+
+output "amazon_worker_private_ips" {
+  value = aws_instance.amazon_linux_workers[*].private_ip
+}
+#output of the pivate Ip 
+output "ubuntu_worker_private_ips" {
+  value = aws_instance.ubuntu_workers[*].private_ip
 }
 
